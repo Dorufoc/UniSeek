bdd33315422949569614cc9b475bb620e7177f36
muxixueliunian <158044517132163.com>
2026-07-28 11:33:29 +0800

    用户管理页面优化

--- uniseek_vue/src/pages/admin/UserManagement.vue
+++ uniseek_vue/src/pages/admin/UserManagement.vue
@@ -149,7 +149,7 @@ onMounted(() => {
         <el-table-column prop="id" label="ID" width="100" />
         <el-table-column prop="phone" label="手机号" width="150" show-overflow-tooltip />
         <el-table-column prop="nickname" label="昵称" width="120" show-overflow-tooltip />
-        <el-table-column label="角色" width="100" align="center">
+        <el-table-column label="角色" min-width="100" align="center">
           <template #default="{ row }">
             <el-tag :type="roleTagType(row.role) as any" size="small">
               {{ roleText(row.role) }}
@@ -164,8 +164,8 @@ onMounted(() => {
           </template>
         </el-table-column>
         <el-table-column prop="creditScore" label="信用分" width="80" align="center" />
-        <el-table-column prop="createTime" label="注册时间" width="183" />
-        <el-table-column prop="lastLoginTime" label="最后登录" width="183" />
+        <el-table-column prop="createTime" label="注册时间" width="200" />
+        <el-table-column prop="lastLoginTime" label="最后登录" width="200" />
         <el-table-column label="状态" width="80" align="center">
           <template #default="{ row }">
             <el-tag :type="row.status === 1 ? 'success' : 'danger'" size="small">
